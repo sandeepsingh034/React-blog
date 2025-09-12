bbba5fd95675129aca566cfa2e845cbb18587a1f
Sandeep Singh <sdsingh1891@gmail.com>
2025-09-12 13:20:21 +0530

    Made components

--- react-blog/src/assets/header.jsx
+++ react-blog/src/assets/header.jsx
@@ -1,9 +1,16 @@
 function Header() {
   return (
     <div>
+      <h3><Work/></h3>
       <h1>This is a header file</h1>
     </div>
   )
+}
+
+function Work (){
+return(
+  <h1> I am working over here </h1>
+)
 }
 
 export default Header;
